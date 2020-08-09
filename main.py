ec752c3c1d7ad5a1cef8ac369077e3853852f251
akoen <arw.koen@gmail.com>
2020-08-09 11:38:26 -0700

    Added example functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import re
 
 import convokit
 from convokit import Corpus, download, TextParser, Utterance, Speaker, Conversation
@@ -12,7 +13,8 @@ from convokit.forecaster.CRAFT import craft_tokenize
 
 MAX_LENGTH = 80
 
-def load_corpus(corpus_name: str) -> "corpus":
+
+def load_corpus(corpus_name: str):
     """Load the given corpus"""
     data_corpus = Corpus(download(corpus_name))
     data_corpus.load_info('utterance', ['parsed'])
@@ -30,9 +32,9 @@ def train_model(corpus: Corpus):
                             forecast_mode="future",
                             convo_structure="linear",
                             text_func=lambda utt: utt.meta["tokens"][:(MAX_LENGTH - 1)],
+                            label_func=lambda utt: int(utt.meta['comment_has_personal_attack']),
                             forecast_attribute_name="prediction", forecast_prob_attribute_name="pred_score",
                             use_last_only=False,
-                            skip_broken_convos=False
                             )
 
     # Tokenize corpus with results from trained model
@@ -66,8 +68,18 @@ def create_corpus(sample_corpus):
 
     return convo_corpus
 
+def get_random_converstation_corpus(data_corpus: Corpus):
+    convo = data_corpus.random_conversation()
+    utterance_list = convo.get_chronological_utterance_list()
 
-def extract_politeness_features(corpus):
+    new_corpus = Corpus(utterances=utterance_list[1:3])
+    for utt in new_corpus.iter_utterances():
+        print(utt.text, end='\n\n')
+
+    return new_corpus
+
+
+def analyze_politeness_features(corpus):
     """Extract politeness strategies features"""
 
     ps = PolitenessStrategies(verbose=1000)
@@ -83,19 +95,29 @@ def extract_politeness_features(corpus):
 
     return corpus
 
+def get_politeness_feature_list(utt: Utterance):
+    """Return a list of politeness features in human-readable format from an utterance."""
 
-def calculate_derailment_probability(craft_model: CRAFTModel, forecaster: Forecaster, test_corpus: Corpus):
+    strategies = []
+    for i in utt.meta['politeness_strategies'].items():
+        if i[1] == 1:
+            match = re.findall(r"(?<===).*?(?===)", i[0])
+            strategies.append(match[0])
+
+    return strategies
+
+
+def calculate_derailement_probability(craft_model: CRAFTModel, forecaster: Forecaster, test_corpus: Corpus):
     """Detect whether conversation will turn awry based on training data"""
 
     # Tokenize test corpus
     for utt in test_corpus.iter_utterances():
         utt.add_meta("tokens", craft_tokenize(craft_model.voc, utt.text))
-        print(utt)
 
-    pred = forecaster.transform(test_corpus)
+    return forecaster.transform(test_corpus)
 
-    forecasts_df = forecaster.summarize(pred)
-    print(forecasts_df.loc['reply'])
+    #forecasts_df = forecaster.summarize(pred)
+    #print(forecasts_df)
 
 
 def main(verbose: ('verbose output', 'flag', 'v')):
@@ -103,10 +125,33 @@ def main(verbose: ('verbose output', 'flag', 'v')):
     is_verbose = verbose
     data_corpus = load_corpus('conversations-gone-awry-corpus')
     craft_model, forecaster = train_model(data_corpus)
-    while(True):
-        convo_corpus = create_corpus(data_corpus)
-        convo_corpus = extract_politeness_features(convo_corpus)
-        calculate_derailment_probability(craft_model, forecaster, convo_corpus)
+
+    with open('output-data.csv', mode='w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+
+        for i in range(50):
+            csv_row = []
+
+            # convo_corpus = create_corpus(data_corpus)
+            convo_corpus = get_random_converstation_corpus(data_corpus)
+            convo_corpus = analyze_politeness_features(convo_corpus)
+            convo_corpus = calculate_derailement_probability(craft_model, forecaster, convo_corpus)
+
+            convo_ids = convo_corpus.get_utterance_ids()
+            prompt = convo_corpus.get_utterance(convo_ids[0])
+            reply = convo_corpus.get_utterance(convo_ids[1])
+            reply_strategies = get_politeness_feature_list(reply)
+
+            if(len(prompt.text) < 400 and len(reply.text) < 400):
+                csv_row.append(prompt.text)
+                csv_row.append(reply.text)
+                csv_row.append(' '.join(reply_strategies))
+                csv_row.append(reply.meta['pred_score'])
+                csv_row.append(prompt)
+                csv_row.append(reply)
+
+                writer.writerow(csv_row)
+
 
 
 if __name__ == "__main__":
